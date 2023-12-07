9fb5534e406fee53237f1805cc70beac6e5bc7df
fan <yangfan@hczq.com>
2023-12-07 16:57:04 +0800

    1

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -2,5 +2,6 @@ public class Main {
     private static final String JAVA_IO_TMPDIR = System.getProperty("java.io.tmpdir");
 
     public static void main(String[] args) {
+        System.out.println(123);
     }
 }
