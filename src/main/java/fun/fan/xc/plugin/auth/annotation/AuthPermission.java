0d1631d5b402f76ec39e2838965c3842612dfc97
fan <yangfan@hczq.com>
2025-01-22 15:02:19 +0800

    权限支持对整个controller设置

--- src/main/java/fun/fan/xc/plugin/auth/annotation/AuthPermission.java
+++ src/main/java/fun/fan/xc/plugin/auth/annotation/AuthPermission.java
@@ -11,7 +11,7 @@ import java.lang.annotation.*;
  */
 @Inherited
 @Documented
-@Target(ElementType.METHOD)
+@Target({ElementType.METHOD, ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface AuthPermission {
     @AliasFor("permission")
